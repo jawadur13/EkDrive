4b3cdce7a23ad0efd1a60db0f797e83761c4c96c
Jawadur Rafid <jawadurrafidrafid@gmail.com>
2026-07-30 04:04:24 +0600

    feat: add download service

--- frontend/src/services/download.ts
+++ frontend/src/services/download.ts
@@ -1,13 +1,13 @@
 import api from './api';
 
 export function getDownloadUrl(fileId: string) {
-  return `/api/v1/files/${fileId}/download`;
+  return `/files/${fileId}/download`;
 }
 
 export async function getFileInfo(fileId: string) {
-  return api.get(`/api/v1/files/${fileId}`).then((r) => r.data);
+  return api.get(`/files/${fileId}`).then((r) => r.data);
 }
 
 export async function getChunkInfo(fileId: string, chunkIndex: number) {
-  return api.get(`/api/v1/files/${fileId}/chunk/${chunkIndex}`).then((r) => r.data);
+  return api.get(`/files/${fileId}/chunk/${chunkIndex}`).then((r) => r.data);
 }
